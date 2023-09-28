9b596e57cb5e781b44ad4cd51728f11ed48e4d41
salman <salman88925@gmail.com>
2023-09-28 20:19:29 +0530

    Auto Push

--- 04-variables.sh
+++ 04-variables.sh
@@ -6,6 +6,6 @@ b=20
 c=30
 
 echo -e "the value of variable a is \e[33m $a \e[0m"
-echo -e "the value of variable b is \e[34 ${b} \e[0m"
+echo -e "the value of variable b is \e[34m ${b} \e[0m"
 echo -e "the value of variable c is \e[35m ${c} \e[0m"
 echo -e "the value of variable d is \e[33m ${d} \e[0m"
