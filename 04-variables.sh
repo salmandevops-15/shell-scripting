de82a8f4492c85d599333fc15debe8dc90694a39
salman <salman88925@gmail.com>
2023-09-28 20:12:59 +0530

    Auto Push

---
+++ 04-variables.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+
+a=10
+b=20
+c=30
+
+echo "the value of variable a is $a"
+echo "the value of variable b is ${b}"
+echo "the value of variable c is ${c}"
+echo "the value of variable d is ${d}"
