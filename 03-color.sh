652d6c9f79c1c5fbdda95935b391362c70898d83
salman <salman88925@gmail.com>
2023-09-28 19:58:36 +0530

    Auto Push

--- 03-color.sh
+++ 03-color.sh
@@ -3,3 +3,6 @@
 
 echo -e "\e[32m this is to show green color \e[0m"
 echo -e "\e[33m this to show yellow color \e[0m"
+echo -e "\e[34m this to show blue color \e[0m"
+echo -e "\e[35m this to show magenta color \e[0m"
+echo -e "\e[36m this to show cyan color \e[0m"
