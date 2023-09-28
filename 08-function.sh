46ee70a86335a37084de972aa270b6436f50b350
salman <salman88925@gmail.com>
2023-09-28 21:59:04 +0530

    Auto Push

--- 08-function.sh
+++ 08-function.sh
@@ -12,6 +12,7 @@ sample(){
 status (){
     echo -e "Good morning and todays date is \e[32m $(date +%F) \e[0m"
     echo -e "no of opened sessions is \e[32m $(who | wc -l) \e[0m"
+    echo -e "load avg of the system in last one min is \e[32m $(uptime | awk -F , '{print $3}' | awk -F : '{print $2}') \e[0m"
 }
 
 sample
