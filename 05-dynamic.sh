ee1ad46b0130dc4a7ff2eaab38c4707c0e6f0c91
salman <salman88925@gmail.com>
2023-09-28 20:48:23 +0530

    Auto Push

--- 05-dynamic.sh
+++ 05-dynamic.sh
@@ -8,4 +8,4 @@ NO_OF_SESSION=$(who|wc -l)
 echo "this to show todays date is $TODAYS_DATE"
 echo -e "this is to show date in color \e[32m $TODAYS_DATE \e[0m"
 
-echo -e "this to show number of session in color \e[33m $NO_OF_SESSION \e[0m"
+echo -e "this to show number of session in color \e[32m $NO_OF_SESSION \e[0m"
