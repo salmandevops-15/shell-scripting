5f3fda8a627f2d31bed073eedf5867b8163434ff
salman <salman88925@gmail.com>
2024-01-08 16:58:54 +0530

    Auto Push

--- 09-practice.sh
+++ 09-practice.sh
@@ -16,6 +16,23 @@ echo -e "\e[36m print cyan color font \e[0m"
 
 echo -e "\e[43;32m print green color font with yellow background \e[0m"
 
+#variables 
+
+#decleration of variables
+
+a=10
+b=11
+d=
+
+
+#to print variables
+
+echo the value of a is $a
+echo the value of b is $b
+echo the value of d is $d
+
+to 
+
 
 
 
