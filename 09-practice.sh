d52e81c5e5dd7f697030d168c15dc1d647bbb460
salman <salman88925@gmail.com>
2024-01-08 16:28:48 +0530

    Auto Push

--- 09-practice.sh
+++ 09-practice.sh
@@ -1,21 +1,6 @@
 #!/bin/bash
 
-sample(){
-    echo "this to show sample function"
-    echo "iam executing sample function"
-    echo "sample function is compeleted"
+echo welcome to devops learinig
+echo we can bag high sal job 
 
-    status
-}
-
-
-status (){
-    echo -e "Good morning and todays date is \e[32m $(date +%F) \e[0m"
-    echo -e "no of opened sessions is \e[32m $(who | wc -l) \e[0m"
-    echo -e "load avg of the system in last one min is \e[32m $(uptime | awk -F , '{print $3}' | awk -F : '{print $2}') \e[0m"
-}
-
-sample
-
-#can you see me jhon cena
 
