597a1bb8383619b6aaebbbc14a3d36e571e8318c
salman <salman88925@gmail.com>
2024-01-08 16:47:29 +0530

    Auto Push

--- 09-practice.sh
+++ 09-practice.sh
@@ -8,4 +8,8 @@ echo -e "line1\n\tline2"
 
 echo -e "\e[32m print green color font \e[0m"
 
+# to print color font with color background
+
+echo -e "\e[42;32m print green color font with yellow background \e[0m"
+
 
