4f8a600fee13d0540f39d7a180278956df88283a
salman <salman88925@gmail.com>
2024-01-08 16:55:15 +0530

    Auto Push

--- 09-practice.sh
+++ 09-practice.sh
@@ -7,9 +7,15 @@ echo -e "line1\n\tline2"
 #to print colors font
 
 echo -e "\e[32m print green color font \e[0m"
+echo -e "\e[33m print yellow color font \e[0m"
+echo -e "\e[34m print brown color font \e[0m"
+echo -e "\e[35m print blue color font \e[0m"
+echo -e "\e[36m print cyan color font \e[0m"
 
 # to print color font with color background
 
 echo -e "\e[43;32m print green color font with yellow background \e[0m"
+
+
 
 
