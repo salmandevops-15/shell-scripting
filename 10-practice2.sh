aaa2344aa77caef7cde22c6fe5fbcea3c038e1c3
salman <salman88925@gmail.com>
2024-01-09 16:08:45 +0530

    Auto Push

--- 10-practice2.sh
+++ 10-practice2.sh
@@ -17,3 +17,25 @@ ppqtwwiyuassb
 COMMENT
 
 echo this is practice2
+
+
+Action=$1
+
+case $Action in 
+
+    start)
+    echo -e "\e[32m starting the rabbit MQ \e[0m"
+    ;;
+    stop)
+    echo -e "\e[31m stopping the rabbitMQ \e[0m"
+    ;;
+    restart)
+    echo -e "\e[34m restarting the rabbit MQ \e[0m"
+    ;;
+    enable)
+    echo -e "\e[36m enabling the rabbit MQ \e[om"
+    ;;
+    *)
+    echo -e "\e[35m the possible inputs are start -- stop -- restrat -- enable \e[0m"
+    ;;
+esac
