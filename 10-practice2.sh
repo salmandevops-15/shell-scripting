9cb942ae82b8f88374ac7adcaaa30b73932bbefb
salman <salman88925@gmail.com>
2024-01-09 15:58:47 +0530

    Auto Push

---
+++ 10-practice2.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+
+#redirectors
+
+#two type of redirectors 
+#1) input redirector < or 1< (ex: sudo mysql < tmp/studentapp.sql)
+#2) output redirector > (overwrite the output of file) and >> (appends the result with old result)
+
+# if we want to save both std output and std error we use &> (ex: ls -ltr &> output.txt)
+
+<<COMMENT
+ hi this IS a multi line comment
+ we can see this part of the script is not executed 
+ppqtwwiyuassb
+
+
+COMMENT
