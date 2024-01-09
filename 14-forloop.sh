4a71d7d8e7d0af0418196ffa005f7a5d1ad276e1
salman <salman88925@gmail.com>
2024-01-09 22:44:59 +0530

    Auto Push

--- 14-forloop.sh
+++ 14-forloop.sh
@@ -8,10 +8,12 @@
     done
 COMMENT
 
+#example 1
 for var in a b c d e f; do   
     echo the value is $var
 done 
 
+#example 2
 for time in 1pm 2pm 3pm 4pm 5pm 6pm; do
     echo "the time is $time"
 done
