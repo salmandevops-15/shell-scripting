6dc5f3dd4c4be90bdb131e701344d8a0cddeaecc
salman <salman88925@gmail.com>
2024-01-09 22:00:19 +0530

    Auto Push

--- 12-ifelse.sh
+++ 12-ifelse.sh
@@ -16,10 +16,31 @@ if [ "$Action" == "start" ] ; then
 
 fi
 
+#if-else statement
+
 if [ "$Action" == "start" ] ; then
-    echo "starting Rabbit MQ"
+    echo -e "\e[32m starting Rabbit MQ \e[0m"
 
 else
-    echo "the possible input is start"
+    echo -e "\e[36m the possible input is start \e[0m"
+
+fi
+
+#else-if
+
+if [ "$Action" == "start" ] ; then
+    echo -e "\e[32m strating RabbitMQ \e[0m"
+
+elif [ "$Action" == "stop" ] ; then
+    echo -e "\e[31m stopping Rabbit MQ \e[0m"
+
+elif [ "$Action" == "restart" ] ; then
+    echo -e "\e[34m restarting Rabbit MQ \e[0m"
+
+elif [ "$Action" == "enable" ] ; then
+    echo -e "\e[35m enabling Rabbit MQ \e[0m"
+
+else 
+    echo -e "\e[36m the possible input are start -- stop -- restart -- enable \e[0m"
 
 fi
