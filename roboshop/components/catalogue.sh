8d6ee5b2e389bec63f7936771c0984c956063e4c
salman <salman88925@gmail.com>
2024-01-16 19:11:56 +0530

    Auto Push

--- roboshop/components/catalogue.sh
+++ roboshop/components/catalogue.sh
@@ -2,26 +2,8 @@
 
 
 COMPONENT=catalogue
-LOGFILE="/tmp/${COMPONENT}.logs"
-APPUSER=roboshop
 
-ID=$(id -u)
-
-if [ $ID -ne 0 ] ; then
-    echo -e "\e[31m This script needs to run by a root user or user with sudo privilage \e[0m"
-    exit 1
-fi
-
-stat () {
-    if [ $1 -eq 0 ] ; then
-    echo -e "\e[32m SUCCESSFUL \e[0m"
-else
-    echo -e "\e[31m FAILURE \e[0m"
-fi
-
-}
-
-echo -e "************ \e[36m Installation of ${COMPONENT} has Started \e[0m ***************"
+source components/common.sh
 
 echo -n "Configuring the ${COMPONENT} repo :"
     curl --silent --location https://rpm.nodesource.com/setup_16.x | sudo bash -        &>> $LOGFILE
