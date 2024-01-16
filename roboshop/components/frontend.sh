3450756e1aa25f23ce15021b6fad5693f11ab493
salman <salman88925@gmail.com>
2024-01-16 18:25:15 +0530

    Auto Push

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -47,10 +47,12 @@ mv localhost.conf /etc/nginx/default.d/roboshop.conf
 stat $?
 
 
-echo -n "Updating Reverse proxy with internal DNS_NAME :"
+echo -n "Updating Reverse proxy with internal DNS_NAME : "
 for component in catalogue user cart shipping payment; do
-sed -i -e '/$component/s/localhost/$component.roboshop.internal/'   /etc/nginx/default.d/roboshop.conf
+sed -i -e "/$component/s/localhost/$component.roboshop.internal/"   /etc/nginx/default.d/roboshop.conf
 done
+
+
 
 echo -n "Starting the ${COMPONENT} :"
 systemctl enable nginx       &>> $LOGFILE
