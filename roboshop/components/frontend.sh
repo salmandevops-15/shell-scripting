f4b47d1cfe14aa423dd0e012cd12d215ab08b197
salman <salman88925@gmail.com>
2024-01-18 18:41:38 +0530

    Auto Push

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -1,25 +1,8 @@
 #!/bin/bash
 
 COMPONENT=frontend
-LOGFILE="/tmp/${COMPONENT}.logs"
 
-ID=$(id -u)
-
-if [ $ID -ne 0 ] ; then
-    echo -e "\e[31m This script needs to run by a root user or user with sudo privilage \e[0m"
-    exit 1
-fi
-
-stat () {
-    if [ $1 -eq 0 ] ; then
-    echo -e "\e[32m SUCCESSFUL \e[0m"
-else
-    echo -e "\e[31m FAILURE \e[0m"
-fi
-
-}
-
-echo -e "************ \e[36m Installation of ${COMPONENT} has Started \e[0m ***************"
+source components/common.sh
 
 
 echo -n "installing nginx:"
