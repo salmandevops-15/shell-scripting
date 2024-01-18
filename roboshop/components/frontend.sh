c942e392cfb735e0c3fcc8671f21816d049b189b
salman <salman88925@gmail.com>
2024-01-18 18:42:21 +0530

    Auto Push

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -48,30 +48,3 @@ echo -e "************ \e[36m Installation of ${COMPONENT} has Completed \e[0m **
 
 
 
-
-# ```
-# # yum install nginx -y
-# # systemctl enable nginx
-# # systemctl start nginx
-
-# ```
-
-# Let's download the HTDOCS content and deploy it under the Nginx path.
-
-# ```
-# # curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
-
-# ```
-
-# Deploy in Nginx Default Location.
-
-# ```
-# # cd /usr/share/nginx/html
-# # rm -rf *
-# # unzip /tmp/frontend.zip
-# # mv frontend-main/* .
-# # mv static/* .
-# # rm -rf frontend-main README.md
-# # mv localhost.conf /etc/nginx/default.d/roboshop.conf
-
-# ```
