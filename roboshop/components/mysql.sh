91ec3ca289ceff3657f5e2b10fcd868e012471cf
salman <salman88925@gmail.com>
2024-01-18 16:01:38 +0530

    Auto Push

--- roboshop/components/mysql.sh
+++ roboshop/components/mysql.sh
@@ -19,20 +19,17 @@ systemctl enable mysqld                     &>> $LOGFILE
 systemctl start mysqld                      &>> $LOGFILE
 stat $?
 
-# echo -n "Fetching default root password : "
-# DEFAULT_ROOT_PASSWORD=$(grep 'temporary password' /var/log/mysqld.log | awk  '{print $NF}')
-# stat $? 
 
 echo -n "fetching the root user default password :"
 DEFAULT_ROOT_PASSWORD=$(grep 'temporary password' /var/log/mysqld.log | awk  '{print $NF}' )      &>> $LOGFILE
 stat $?
 
-# echo "show databases;" | mysql -uroot -pRoboShop@1
-# if [ $? -ne 0 ]; then
-# echo -n "Updating the Root user password with std password:"
-# echo "ALTER USER 'root'@'localhost' IDENTIFIED BY RoboShop@1;" | mysql --connect-expired-password -uroot -p${DEFAULT_ROOT_PASSWORD}   &>> $LOGFILE
-# stat $?
-# fi
+echo "show databases;" | mysql -uroot -pRoboShop@1
+if [ $? -ne 0 ]; then
+echo -n "Updating the Root user password with std password:"
+echo "ALTER USER 'root'@'localhost' IDENTIFIED BY RoboShop@1;" | mysql --connect-expired-password -uroot -p${DEFAULT_ROOT_PASSWORD}   &>> $LOGFILE
+stat $?
+fi
 
 # echo "show plugins;" | mysql -uroot -pRoboShop@1 | grep validate_password
 # if [ $? -eq 0 ]; then
