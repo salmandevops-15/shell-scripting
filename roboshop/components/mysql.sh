9288cc23115955f558130df5b162e015f3ba4c80
salman <salman88925@gmail.com>
2024-01-18 16:04:55 +0530

    Auto Push

--- roboshop/components/mysql.sh
+++ roboshop/components/mysql.sh
@@ -24,19 +24,13 @@ echo -n "fetching the root user default password :"
 DEFAULT_ROOT_PASSWORD=$(grep 'temporary password' /var/log/mysqld.log | awk  '{print $NF}' )      &>> $LOGFILE
 stat $?
 
-echo "show databases;" | mysql -uroot -pRoboShop@1 &>> $LOGFILE
-if [ $? -ne 0 ] ; then 
-    echo -n "Performing password reset of root user:"
-    echo "ALTER USER 'root'@'localhost' IDENTIFIED BY 'RoboShop@1';" | mysql --connect-expired-password -uroot -p${DEFAULT_ROOT_PASSWORD}   &>> $LOGFILE
-    stat $?
-fi 
 
-# echo "show databases;" | mysql -uroot -pRoboShop@1
-# if [ $? -ne 0 ]; then
-# echo -n "Updating the Root user password with std password:"
-# echo "ALTER USER 'root'@'localhost' IDENTIFIED BY 'RoboShop@1';" | mysql --connect-expired-password -uroot -p${DEFAULT_ROOT_PASSWORD}   &>> $LOGFILE
-# stat $?
-# fi
+echo "show databases;" | mysql -uroot -pRoboShop@1
+if [ $? -ne 0 ]; then
+echo -n "Updating the Root user password with std password:"
+echo "ALTER USER 'root'@'localhost' IDENTIFIED BY 'RoboShop@1';" | mysql --connect-expired-password -uroot -p${DEFAULT_ROOT_PASSWORD}   &>> $LOGFILE
+stat $?
+fi
 
 # echo "show plugins;" | mysql -uroot -pRoboShop@1 | grep validate_password
 # if [ $? -eq 0 ]; then
