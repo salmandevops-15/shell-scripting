2d557d10861386b699d3f9f1bc9439dd6dab991b
salman <salman88925@gmail.com>
2024-01-20 13:49:25 +0530

    Auto Push

--- roboshop/create_ec2.sh
+++ roboshop/create_ec2.sh
@@ -2,8 +2,19 @@
 
 AMI_ID=$(aws ec2 describe-images --region us-east-1 --image-ids ami-0f75a13ad2e340a58 | jq '.Images[].ImageId' | sed 's/"//g')
 
+SG_ID=$(aws ec2 describe-security-groups --group-ids sg-04b697d4f9bf8c941 | jq '.SecurityGroups[].GroupId' | sed 's/"//g')
+
 echo -e "\e[36m The value of AMI-ID is $AMI_ID  \e[0m"
+echo -e "\e[36m The value of SG-ID is $SG_ID  \e[0m"
 
-# aws ec2 describe-images --region us-east-1 --image-ids ami-0f75a13ad2e340a58
 
-# aws ec2 run-instances --image-id ami-xxxxxxxx --count 1 --instance-type t2.micro  --security-group-ids sg-903004f8
+# aws ec2 describe-security-groups --group-ids sg-04b697d4f9bf8c941 | jq '.SecurityGroups[].GroupId' | sed 's/"//g'
+
+# aws ec2 describe-security-groups --group-ids sg-04b697d4f9bf8c941
+
+
+
+
+# aws ec2 describe-images --region us-east-1 --image-ids ami-0f75a13ad2e340a58            ## command to fetch ami details
+
+# aws ec2 run-instances --image-id ami-xxxxxxxx --count 1 --instance-type t2.micro  --security-group-ids sg-903004f8         ## command to create ec2 instance
