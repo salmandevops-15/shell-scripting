2a3f9b6599551dcbd6a5080db049b25acfe69bc6
salman <salman88925@gmail.com>
2024-01-20 14:51:24 +0530

    Auto Push

--- roboshop/create_ec2.sh
+++ roboshop/create_ec2.sh
@@ -2,15 +2,19 @@
 
 COMPONENT=$1
 
+
 if [ -z $1 ] ; then
 
     echo -e "\e[31m please give input \e[0m"
     echo -e "\e[36m the possible input is: \n\t\t sudo bash create_ec2.sh component_name \e[0m"
 fi
 
+
 AMI_ID=$(aws ec2 describe-images --region us-east-1 --image-ids ami-0f75a13ad2e340a58 | jq '.Images[].ImageId' | sed 's/"//g')
 
 SG_ID=$(aws ec2 describe-security-groups --group-ids sg-04b697d4f9bf8c941 | jq '.SecurityGroups[].GroupId' | sed 's/"//g')
+
+
 
 CREATE_EC2 () {
 
@@ -26,12 +30,17 @@ CREATE_EC2 () {
 
 
 if [ "$1" = "all" ] ; then
-for component in frontend mongodb catalogue redis user cart mysql shipping  rabbitmq payment ; do
 
-        COMPONENT=$component
-        
-        CREATE_EC2
-done
+    for component in frontend mongodb catalogue redis user cart mysql shipping  rabbitmq payment ; do
+
+            COMPONENT=$component
+            
+            CREATE_EC2
+    done
+
+else 
+
+    CREATE_EC2
 
 fi
 
