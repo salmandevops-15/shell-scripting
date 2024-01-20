8bc492cb53161b08e2ffde0c98580ac2cdf60e55
salman <salman88925@gmail.com>
2024-01-20 15:44:28 +0530

    Auto Push

--- roboshop/create_ec2.sh
+++ roboshop/create_ec2.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 COMPONENT=$1
+ENV=$2
+HOSTEDZONE_ID=Z079795913TEES271E21K
 
 
 if [ -z $1 ] ; then
@@ -16,6 +18,9 @@ AMI_ID=$(aws ec2 describe-images --region us-east-1 --image-ids ami-0f75a13ad2e3
 
 SG_ID=$(aws ec2 describe-security-groups --group-ids sg-04b697d4f9bf8c941 | jq '.SecurityGroups[].GroupId' | sed 's/"//g')
 
+IPADDRESS=$(aws ec2 run-instances  --image-id ${AMI_ID}  --instance-type t3.micro   --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$COMPONENT-$ENV}]" | jq '.Instances[].PrivateIpAddress' | sed -e 's/"//g')
+
+
 
 
 CREATE_EC2 () {
@@ -24,9 +29,19 @@ CREATE_EC2 () {
         echo -e "\e[36m The value of SG-ID is $SG_ID  \e[0m"
 
 
-        echo -e "*********** \e[32m LAUNCHING Ec2 instance \e[0m ****************"
 
+        echo -e "*********** \e[32m LAUNCHING Ec2 instance \e[0m ****************"
         aws ec2 run-instances --image-id $AMI_ID --instance-type t2.micro  --count 1 --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$COMPONENT}]" | jq .
+        echo -e "*********** \e[32m LAUNCHING Ec2 instance is Completed \e[0m ****************" 
+        
+
+        echo -e "Private Ip address of $COMPONENT-$ENV server is \e[36m $IPADDRESS \e[0m"
+
+
+        echo -e "\e[36m ***** Creating DNS record for the $COMPONENT-$ENV  ****** \e[0m"
+        sed -e "s/COMPONENT/${COMPONENT}-${ENV}/"  -e "s/IPADDRESS/${IPADDRESS}/" route53.json > /tmp/record.json
+        aws route53 change-resource-record-sets --hosted-zone-id $HOSTEDZONEID --change-batch file:///tmp/record.json
+        echo -e "\e[36m **** Creating DNS Record for the $COMPONENT has completed **** \e[0m"
 
 }
 
