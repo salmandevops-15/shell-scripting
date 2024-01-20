69f8e0d53f93f1b1b2aba09203c286483c246c3b
salman <salman88925@gmail.com>
2024-01-20 14:25:56 +0530

    Auto Push

--- roboshop/create_ec2.sh
+++ roboshop/create_ec2.sh
@@ -10,12 +10,12 @@ echo -e "\e[36m The value of AMI-ID is $AMI_ID  \e[0m"
 echo -e "\e[36m The value of SG-ID is $SG_ID  \e[0m"
 
 
-# echo -e "*********** \e[32m LAUNCHING Ec2 instance \e[0m ****************"
-# aws ec2 run-instances --image-id $AMI_ID --count 1 --instance-type t2.micro  --security-group-ids $SG_ID | jq .   
-
+echo -e "*********** \e[32m LAUNCHING Ec2 instance \e[0m ****************"
 
 aws ec2 run-instances --image-id $AMI_ID --instance-type t2.micro  --count 1 --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$COMPONENT}]" | jq .
 
+
+# aws ec2 run-instances --image-id $AMI_ID --count 1 --instance-type t2.micro  --security-group-ids $SG_ID | jq .   
 
 
 # aws ec2 describe-security-groups --group-ids sg-04b697d4f9bf8c941 | jq '.SecurityGroups[].GroupId' | sed 's/"//g'
